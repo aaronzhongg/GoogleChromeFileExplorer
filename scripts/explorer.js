670612b060c0911785e19c3d362f85e39d47d490
Zhiage <noemail@example.com>
2017-04-15 13:22:32 +1200

    add examples of adding classes to existing html elements, wrapping existing elements in divs and appending html

--- scripts/explorer.js
+++ scripts/explorer.js
@@ -1,13 +1,21 @@
-console.log("this is injected");
+console.log("explorer.js injected");
 
+//Get existing html elements and give them classes to style them
+$("table").addClass('table-striped');
+$("table").wrap('<div class="explorer-right"></div>');
+$("body").append('<div class="explorer-left"></div>')
 
-console.log("DOM fully loaded and parsed");
-var test = document.createElement("tr");
-var testNode = document.createElement("td");
-testNode.dataset.value = "TESTERINO";
-test.appendChild(testNode);
-test.appendChild(testNode);
-test.appendChild(testNode);
-
-var table = document.getElementById("tbody");
-table.appendChild(test);
+// console.log("DOM fully loaded and parsed");
+// var test = document.createElement("tr");
+// var testNode = document.createElement("td");
+// testNode.dataset.value = "TESTERINO";
+// test.appendChild(testNode);
+// var testNode1 = document.createElement("td");
+// testNode1.dataset.value = "TESTERINO";
+// test.appendChild(testNode1);
+// var testNode2 = document.createElement("td");
+// testNode2.dataset.value = "TESTERINO";
+// test.appendChild(testNode2);
+// console.log(test);
+// var table = document.getElementById("tbody");
+// table.appendChild(test);
